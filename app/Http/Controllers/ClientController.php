5f45b50891c2b6cc782ecadba3d8eb6e867ab9a8
Diego Goulart <diego@djgoulart.com.br>
2016-05-06 02:01:24 -0300

    Add Client Restful routes and controller

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Models\Client;
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+
+class ClientController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([Client::all()]);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response()->json([Client::create($request->all())]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json([Client::find($id)]);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //Client::where('id',$id)->update($request->all())
+
+        return response()->json([Client::where('id',$id)->update($request->all())]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return response()->json([Client::where('id',$id)->delete()]);
+    }
+}
